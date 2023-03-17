efeba0171c8e1aa0d264c1fe169897055f95e4b8
Vlads Muravjovs <vlads.muravjovs@gmail.com>
2023-03-17 12:32:34 +0200

    Navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
+import React, { useState } from "react";
+import Link from 'next/link'
 import { FaBars, FaTimes } from "react-icons/fa";
-import { useState } from "react";
 
 function Navbar() {
     const [open, setOpen] = useState(false)
@@ -8,10 +9,10 @@ function Navbar() {
         <nav>
             <h3>Logo</h3>
             <div className={open ? 'responsive_nav' : ''}>
-                <a href="/#">Home</a>
-                <a href="/#">Order a taxi</a>
-                <a href="/#">Info</a>
-                <a href="/#">Contact us</a>
+                <Link href="/">Home</Link>
+                <Link href="/">Order a taxi</Link>
+                <Link href="/">Info</Link>
+                <Link href="/">Contact us</Link>
                 <button className='nav-btn nav-close-btn' onClick={() => setOpen(!open)}>
                     <FaTimes />
                 </button>
