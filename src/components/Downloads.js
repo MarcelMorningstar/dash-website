0161bbacd31fcff79e85338dd0f684045fa91bbf
Vlads Muravjovs <vlads.muravjovs@gmail.com>
2023-03-20 13:46:43 +0200

    Window animations

--- src/components/Downloads.js
+++ src/components/Downloads.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 import styles from '../styles/Downloads.module.css';
 
@@ -12,24 +13,53 @@ export default function Downloads(){
                 marginTop:50,
                 height:'100%'
             }}>
-                <div style={{
-                    display:'flex',
-                    gap:10,
-                    justifyContent:'space-evenly',
-                    width:'100%',
-                    height:'80%'
+                <div 
+                    style={{
+                        display:'flex',
+                        gap:10,
+                        justifyContent:'space-evenly',
+                        width:'100%',
+                        height:'80%'
                 }}>
-                    <DownloadsWindow title="Klients">
-                        <h1>DASH Klientu Aplikācija</h1>
-                        <br />
+                    <DownloadsWindow 
+                        title="Klients"
+                        animProps={{
+                            'initial':{
+                                x:-150,
+                                opacity:0
+                            },
+                            'whileInView':{
+                                x:0,
+                                opacity:1
+                            },
+                            'transition':{
+                                duration:0.75
+                            }
+                        }}
+                    >
                         <p style={{
                             textAlign:'left'
                         }}>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                         <br />
                         <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
                     </DownloadsWindow>
-                    <DownloadsWindow title="Šoferis">
-                        <h1>yaha</h1>
+                    <DownloadsWindow 
+                        title="Šoferis"
+                        animProps={{
+                            'initial':{
+                                x:150,
+                                opacity:0
+                            },
+                            'whileInView':{
+                                x:0,
+                                opacity:1
+                            },
+                            'transition':{
+                                duration:0.75
+                            }
+                        }}
+                    >
+                        <p>interesanta informācija</p>
                     </DownloadsWindow>
                 </div>
             </div>
@@ -39,10 +69,15 @@ export default function Downloads(){
 
 function DownloadsWindow(props){
     return (
-        <div className={styles['download-window']} style={{
-            width:'max(300px, 40%)',
-            backgroundColor:'rgb(240,240,240)'
-        }}>
+        <motion.div className={styles['download-window']} 
+            initial={props.animProps['initial']}
+            whileInView={props.animProps['whileInView']}
+            transition={props.animProps['transition']}
+            style={{
+                width:'max(300px, 40%)',
+                backgroundColor:'rgb(240,240,240)'
+            }}
+        >
             <div style={{
                 display:'flex',
                 justifyContent:'center',
@@ -74,6 +109,6 @@ function DownloadsWindow(props){
                     </div>
                 </div>
             </div>
-        </div>
+        </motion.div>
     )
 }
